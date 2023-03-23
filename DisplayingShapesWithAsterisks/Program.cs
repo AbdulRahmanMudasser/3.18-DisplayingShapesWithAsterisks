a37dc86dc0389f9554209bac97e15db5e913e5c2
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-23 07:22:33 +0500

    Add project files.

---
+++ DisplayingShapesWithAsterisks/Program.cs
@@ -0,0 +1,64 @@
+﻿class DisplayingShapesWithAsterisks
+{
+    static void Main(string [] args)
+    {
+        DisplayingShapesWithAsterisks displayingShapesWithAsterisks = new DisplayingShapesWithAsterisks();
+
+        displayingShapesWithAsterisks.displayBox();
+
+        Console.WriteLine();    // just for spacing purpose
+
+        displayingShapesWithAsterisks.displayOval();
+
+        Console.WriteLine();    // just for spacing purpose
+
+
+    }
+    // end of Main method
+
+    void displayBox()
+    {
+        Console.WriteLine("Displaying Diamond\n");
+
+        Console.WriteLine("*********");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*********");
+    }
+    // end of displayDiamond method
+
+    void displayOval()
+    {
+        Console.WriteLine("Displaying Oval\n");
+
+        Console.WriteLine("   ***   ");
+        Console.WriteLine(" *     * ");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine("*       *");
+        Console.WriteLine(" *     * ");
+        Console.WriteLine("   ***   ");
+    }
+    // end of displayOval method
+
+    void displayArrow()
+    {
+
+    }
+    // end of displayArrow method
+
+    void displayDiamond() 
+    {
+
+    }
+    // end of displayDiamond method
+
+}
+// end of DisplayingShapesWithAsterisks class
